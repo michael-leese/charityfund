7d367d5f73f303eef0164a7a4402d95478ff6f43
Michael Leese <michael-leese@outlook.com>
2020-01-18 18:52:06 +0000

    add accounts app and create index page

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-
-def say_hello(request):
-    return HttpResponse("Hello World")
+def index(request):
+    return render(request, "index.html")
--- charity_fund/urls.py
+++ charity_fund/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from accounts.views import say_hello
+from accounts.views import index
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', say_hello),
+    url(r'^$', index),
 ]
