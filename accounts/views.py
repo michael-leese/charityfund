d3e6526f88b80e68ad8b39a61c4029c0a80ccc7e
Michael Leese <michael-leese@outlook.com>
2020-01-18 19:13:14 +0000

    create a login page aand setup hyperlinks

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect, reverse
+from django.contrib import auth, messages
 
 # Create your views here.
+#setup the index page
 def index(request):
     return render(request, "index.html")
+
+#setup the logout and redirect
+def logout(request):
+    auth.logout(request)
+    messages.success(request, "You have successfully logged out!")
+    return redirect(reverse('index'))
+
+def login(request):
+    return render(request, "login.html")
