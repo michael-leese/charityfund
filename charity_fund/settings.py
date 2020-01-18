f1c3178d98f64d87b36f94e91b4adc2246c2620b
Michael Leese <michael-leese@outlook.com>
2020-01-18 20:06:44 +0000

    create base template and change previous pages to use template inheritance, which is how each will be built from now on

--- charity_fund/settings.py
+++ charity_fund/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'charity_fund.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
