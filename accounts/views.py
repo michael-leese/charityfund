6fe9d499e1d1c87910746a7234c8f7cf338fb7fc
Michael Leese <michael-leese@outlook.com>
2020-01-18 20:25:15 +0000

    add authentication to login and logout page

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse, redirect, reverse
 from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 from accounts.forms import UserLoginForm
 # Create your views here.
 #Return the index page
@@ -7,6 +8,7 @@ def index(request):
     return render(request, "index.html")
 
 #User logout and redirect
+@login_required
 def logout(request):
     auth.logout(request)
     messages.success(request, "You have successfully logged out!")
@@ -14,6 +16,9 @@ def logout(request):
 
 #Return the login page
 def login(request):
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+
     if request.method == "POST":
         login_form = UserLoginForm(request.POST)
 
@@ -24,6 +29,7 @@ def login(request):
             if user:
                 auth.login(user=user, request=request)
                 messages.success(request, "You have successfully logged in!")
+                return redirect(reverse('index'))
             else:
                 login_form.add_error(None, "Your username or password is incorrect!")
     else:
