273b3d168ff073387159d074a3700337926d5800
Michael Leese <michael-leese@outlook.com>
2020-04-17 01:52:59 +0100

    update search function to handle the all_appeals template correctly

--- search/views.py
+++ search/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
+from accounts.models import Org
 from appeals.models import Appeal
 from taggit.models import Tag
 
 def do_search(request):
     '''
-    Search Fucntion
+    Search Function
     '''
     all_appeals = Appeal.objects.filter(tags__name__icontains=request.GET['search'])
-    print(all_appeals)
-    return render(request, "all_appeals.html", {"all_appeals": all_appeals})
+    active = "active"
+    if request.user.is_authenticated:
+        org = Org.objects.filter(user=request.user)
+        if org:
+            hasOrg = True
+            return render(request, 'all_appeals.html', {'all_appeals': all_appeals, 'hasOrg': hasOrg, 'active6': active}) 
+        else:
+            hasOrg = False
+            return render(request, 'all_appeals.html', {'all_appeals': all_appeals, 'hasOrg': hasOrg, 'active6': active})
+    else:
+        hasOrg = False
+        return render(request, 'all_appeals.html', {'all_appeals': all_appeals, 'hasOrg': hasOrg, 'active6': active})
