31f68bc3b1389e80374c1f1f547a731720b0daa2
Michael Leese <michael-leese@outlook.com>
2020-04-26 01:58:28 +0100

    add edit appeal fucntionality

--- appeals/views.py
+++ appeals/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, HttpResponse, redirect, reverse, get_object_or_404
+from django.shortcuts import render, HttpResponse, redirect, reverse, get_object_or_404, HttpResponseRedirect
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from accounts.models import User, Org
 from appeals.models import Appeal
@@ -10,7 +11,7 @@ from appeals.forms import AppealForm
 from django.utils import timezone
 from appeals.serializers import AppealsSerializer
 
-# Create an appeal
+@login_required
 def create_appeal(request):
     """
     Ensures create appeal form is valid and pushes data into
@@ -42,6 +43,37 @@ def create_appeal(request):
     else:
         return redirect(reverse('index'))
 
+@login_required
+def edit_appeal(request):
+    """
+    Ensures create appeal form is valid and pushes data into
+    a new appeal, then redirects to appeals list page
+    """
+    if request.user.is_authenticated:
+        owner = False
+        previous = request.GET.get('next', '/')
+        instance = get_object_or_404(Appeal, id=request.GET.get('id'))
+        org = Org.objects.filter(user=request.user)
+        form = AppealForm(request.POST or None, instance=instance)
+        if request.method == "POST":
+            if form.is_valid():
+                appeal = form.save(commit=False)
+                appeal.author = request.user
+                appeal.org = Org.objects.get(user=request.user)
+                appeal.created_date = timezone.now()
+                appeal.save()
+                form.save_m2m()
+                messages.success(request, "Congratulations you have edited appeal called " + instance.title)
+                return HttpResponseRedirect(previous)
+            else:
+                messages.error(request, "Unable to edit at this time.")
+                return render(request, 'edit_appeal.html', {'form': form, 'previous': previous, 'instance': instance})
+        else:
+            return render(request, 'edit_appeal.html', {'form': form, 'previous': previous, 'instance': instance})
+    else:
+        active = "active"
+        return render(request, 'index.html', {'active1': active})
+
 def show_all_appeals(request):
     """
     Gets the appeals
@@ -60,22 +92,28 @@ def show_all_appeals(request):
         hasOrg = False
         return render(request, 'all_appeals.html', {'all_appeals': all_appeals, 'hasOrg': hasOrg, 'active6': active})
 
+@login_required
 def single_appeal(request):
     """
     Gets the single appeal clicked on by a user
     """
     if request.user.is_authenticated:
         active = "active"
+        owner = False
         org = Org.objects.filter(user=request.user)
         appeal = Appeal.objects.get(id=request.GET.get('id'))
+        creator = appeal.author.id
+        print(creator)
+        if creator is request.user.id:
+            owner = True
         orders = Order.objects.filter(appeal=appeal.id).order_by('-created_date')
         calcPercent = progress_perc(appeal.money_raised, appeal.money_target)
         if org:
             hasOrg = True
-            return render(request, 'single_appeal.html', {'appeal': appeal, 'orders': orders, 'hasOrg': hasOrg, 'calcPercent': calcPercent}) 
+            return render(request, 'single_appeal.html', {'appeal': appeal, 'orders': orders, 'hasOrg': hasOrg, 'calcPercent': calcPercent, 'owner': owner}) 
         else:
             hasOrg = False
-            return render(request, 'single_appeal.html', {'appeal': appeal, 'orders': orders,  'hasOrg': hasOrg, 'calcPercent': calcPercent}) 
+            return render(request, 'single_appeal.html', {'appeal': appeal, 'orders': orders,  'hasOrg': hasOrg, 'calcPercent': calcPercent, 'owner': owner}) 
     else:
         return redirect(reverse('index'))
 
