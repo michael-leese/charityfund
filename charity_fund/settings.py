9ce4c3b514995d02d13d3562731d7fa921ed2b41
Michael Leese <michael-leese@outlook.com>
2020-04-27 18:30:03 +0100

    try staticfiles for static folders

--- charity_fund/settings.py
+++ charity_fund/settings.py
@@ -150,6 +150,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
