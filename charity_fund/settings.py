8ed6d35792e0fbe598a8c2cc29f417ec24ba228d
Michael Leese <michael-leese@outlook.com>
2020-04-30 19:07:20 +0100

    remove static root

--- charity_fund/settings.py
+++ charity_fund/settings.py
@@ -174,7 +174,7 @@ MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
