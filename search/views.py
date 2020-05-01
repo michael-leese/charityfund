e1c78efb3faf3e86d5ee5cfd201f66700adf20b5
Michael Leese <michael-leese@outlook.com>
2020-05-02 00:45:41 +0100

    add features to search results when none and add profile images to all pages to serve in the navbar

--- search/views.py
+++ search/views.py
@@ -10,23 +10,24 @@ def do_search(request):
     previous = request.GET.get('next', '/')
     search = request.GET['search']
     if search is "":
-        print("blank search")
         if previous is None:
             return redirect(reverse('index'))
         elif previous is not None:
             return HttpResponseRedirect(previous)
     elif search is not "":
-        print(search)
         all_appeals = Appeal.objects.filter(tags__name__icontains=search).distinct()
         active = "active"
+        no_search_back = False
+        if all_appeals.count() is 0:
+            no_search_back = True
         if request.user.is_authenticated:
             org = Org.objects.filter(user=request.user)
             if org:
                 hasOrg = True
-                return render(request, 'all_appeals.html', {'all_appeals': all_appeals, 'hasOrg': hasOrg, 'active6': active}) 
+                return render(request, 'all_appeals.html', {'all_appeals': all_appeals, 'hasOrg': hasOrg, 'active6': active, 'search': search, 'no_search_back': no_search_back}) 
             else:
                 hasOrg = False
-                return render(request, 'all_appeals.html', {'all_appeals': all_appeals, 'hasOrg': hasOrg, 'active6': active})
+                return render(request, 'all_appeals.html', {'all_appeals': all_appeals, 'hasOrg': hasOrg, 'active6': active, 'search': search, 'no_search_back': no_search_back})
         else:
             hasOrg = False
-            return render(request, 'all_appeals.html', {'all_appeals': all_appeals, 'hasOrg': hasOrg, 'active6': active})
+            return render(request, 'all_appeals.html', {'all_appeals': all_appeals, 'hasOrg': hasOrg, 'active6': active, 'search': search, 'no_search_back': no_search_back})
