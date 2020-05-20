56718b54f74eaa99bced60dc35961bd107eeb137
michael-leese <43089779+michael-leese@users.noreply.github.com>
2020-05-21 00:43:57 +0100

    try to fix static issue

--- charity_fund/settings.py
+++ charity_fund/settings.py
@@ -187,6 +187,7 @@ DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 # MEDIA_URL = '/media/'
